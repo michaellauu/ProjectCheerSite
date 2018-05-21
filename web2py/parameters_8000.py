a326baf589e3a72e015d60478552f407f64b7564
awong35 <allanwong98@gmail.com>
2018-05-20 21:35:23 -0700

    created part of the web layout and got menu buttons to work properly

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$bffe0d3667d5ae5d$afb3f32eb68b2b575d2a52fccb2c8af8664908ed"
+password="pbkdf2(1000,20,sha512)$936b5b8dc150ec31$9dc918976ca4f209b2675eddedc8bc24fcb00ee2"
