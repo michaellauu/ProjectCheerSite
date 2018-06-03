8f3bc37e77e979846008245c69887ea8cb5c8a19
awong35 <allanwong98@gmail.com>
2018-06-03 15:12:19 -0700

    added upload code but link to storage does not work yet

--- web2py/applications/start/static/js/default_index.js
+++ web2py/applications/start/static/js/default_index.js
@@ -13,18 +13,69 @@ var app = function() {
         }
     };
 
+        // Enumerates an array.
+        var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
+
+        self.open_uploader = function () {
+            $("div#uploader_div").show();
+            self.vue.is_uploading = true;
+        };
+    
+        self.close_uploader = function () {
+            $("div#uploader_div").hide();
+            self.vue.is_uploading = false;
+            $("input#file_input").val(""); // This clears the file choice once uploaded.
+    
+        };
+    
+        self.upload_file = function (event) {
+            // Reads the file.
+            var input = event.target;
+            var file = input.files[0];
+            if (file) {
+                // First, gets an upload URL.
+                console.log("Trying to get the upload url");
+                $.getJSON('https://upload-dot-luca-teaching.appspot.com/start/uploader/get_upload_url',
+                    function (data) {
+                        // We now have upload (and download) URLs.
+                        var put_url = data['signed_url'];
+                        var get_url = data['access_url'];
+                        console.log("Received upload url: " + put_url);
+                        // Uploads the file, using the low-level interface.
+                        var req = new XMLHttpRequest();
+                        req.addEventListener("load", self.upload_complete(get_url));
+                        // TODO: if you like, add a listener for "error" to detect failure.
+                        req.open("PUT", put_url, true);
+                        req.send(file);
+                    });
+            }
+        };
+    
+    
+        self.upload_complete = function(get_url) {
+            // Hides the uploader div.
+            self.close_uploader();
+            console.log('The file was uploaded; it is now available at ' + get_url);
+            // TODO: The file is uploaded.  Now you have to insert the get_url into the database, etc.
+        };
+
     // Complete as needed.
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
+            is_uploading: false,
+            self_page: true
         },
         methods: {
+            open_uploader: self.open_uploader,
+            close_uploader: self.close_uploader,
+            upload_file: self.upload_file
         }
 
     });
-
+    $("#vue-div").show()
 
     return self;
 };
