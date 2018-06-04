65f8efe8a55b0dc54a57688c975c176077674eaa
MICHAEL-PC\Micha <mlau10@ucsc.edu>
2018-06-04 02:08:00 -0700

    api and js

--- web2py/applications/start/controllers/api.py
+++ web2py/applications/start/controllers/api.py
@@ -1,1 +1,91 @@
+#import tempfile
+
+# Cloud-safe of uuid, so that many cloned servers do not all use the same uuids.
+#from gluon.utils import web2py_uuid
+
 # Here go your api methods.
+
+
+def get_user_name_from_email(email):
+    """Returns a string corresponding to the user first and last names,
+    given the user email."""
+    u = db(db.auth_user.email == email).select().first()
+    if u is None:
+        return 'None'
+    else:
+        return ' '.join([u.first_name, u.last_name])
+
+
+@auth.requires_signature(hash_vars=False)
+def get_images():
+    start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+    end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+    images = []
+    has_more = False
+    rows = db(db.user_images.user_email == auth.user.email).select(db.user_images.ALL, limitby=(start_idx, end_idx + 1), orderby=~db.user_images.id)
+
+    for i, r in enumerate(rows):
+        if i < end_idx - start_idx:
+            m = dict(
+                id = r.id,
+                user_email = r.user_email,
+                image_url = r.image_url
+            )
+            images.append(m)
+        else:
+            has_more = True
+    logged_in = auth.user is not None
+    return response.json(dict(images=images, looged_in=logged_in, has_more=has_more,))
+
+
+def get_users():
+    users = []
+    if auth.user is not None:
+        rows = db(db.auth_user.email != auth.user.email).select(db.auth_user.ALL)
+    else:
+        rows = db().select(db.auth_user.ALL)
+
+    for i, r in enumerate(rows):
+        m = dict(
+            id=r.id,
+            user_email=r.email,
+            user_name=get_user_name_from_email(r.email)
+        )
+        users.append(m)
+
+    return response.json(dict(users=users,))
+
+
+def get_user_images():
+    start_idx = int(request.vars.start_idx) if request.vars.start_idx is not None else 0
+    end_idx = int(request.vars.end_idx) if request.vars.end_idx is not None else 0
+    user_email = request.vars.user_email if request.vars.user_email is not None else ""
+
+    has_more = False
+    images = []
+    rows = db(db.user_images.user_email == user_email).select(db.user_images.ALL, limitby=(start_idx, end_idx + 1),  orderby=~db.user_images.id)
+
+    for i, r in enumerate(rows):
+        if i < end_idx - start_idx:
+            m = dict(
+                id = r.id,
+                user_email = r.user_email,
+                image_url = r.image_url
+            )
+            images.append(m)
+        else:
+            has_more = True
+    logged_in = auth.user is not None
+    return response.json(dict(images=images, looged_in=logged_in, has_more=has_more,))
+
+@auth.requires_signature()
+def add_image():
+    m_id = db.user_images.insert(
+        image_url = request.vars.image_url
+    )
+    m = db.user_images(m_id)
+    image = dict(
+        id = m.id,
+        image_url = m.image_url
+    )
+    return response.json(dict(image=image))
--- web2py/applications/start/static/js/default_index.js
+++ web2py/applications/start/static/js/default_index.js
@@ -1,5 +1,6 @@
 // This is the js for the default/index.html view.
 
+
 var app = function() {
 
     var self = {};
@@ -13,70 +14,139 @@ var app = function() {
         }
     };
 
-        // Enumerates an array.
-        var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
+    // Enumerates an array.
+    var enumerate = function(v) { var k=0; return v.map(function(e) {e._idx = k++;});};
 
-        self.open_uploader = function () {
-            $("div#uploader_div").show();
-            self.vue.is_uploading = true;
+    function get_images_url(start_idx, end_idx) {
+        var pp = {
+            start_idx: start_idx,
+            end_idx: end_idx
         };
-    
-        self.close_uploader = function () {
-            $("div#uploader_div").hide();
-            self.vue.is_uploading = false;
-            $("input#file_input").val(""); // This clears the file choice once uploaded.
-    
-        };
-    
-        self.upload_file = function (event) {
-            // Reads the file.
-            var input = event.target;
-            var file = input.files[0];
-            if (file) {
-                // First, gets an upload URL.
-                console.log("Trying to get the upload url");
-                $.getJSON('https://upload-dot-luca-teaching.appspot.com/start/uploader/get_upload_url',
-                    function (data) {
-                        // We now have upload (and download) URLs.
-                        var put_url = data['signed_url'];
-                        var get_url = data['access_url'];
-                        console.log("Received upload url: " + put_url);
-                        // Uploads the file, using the low-level interface.
-                        var req = new XMLHttpRequest();
-                        req.addEventListener("load", self.upload_complete(get_url));
-                        // TODO: if you like, add a listener for "error" to detect failure.
-                        req.open("PUT", put_url, true);
-                        req.send(file);
-                    });
-            }
-        };
-    
-    
-        self.upload_complete = function(get_url) {
-            // Hides the uploader div.
-            self.close_uploader();
-            console.log('The file was uploaded; it is now available at ' + get_url);
-            // TODO: The file is uploaded.  Now you have to insert the get_url into the database, etc.
-        };
+        return images_url + "&" + $.param(pp);
+    }
 
-    // Complete as needed.
+    self.get_images = function () {
+        $.getJSON(get_images_url(0, 20), function (data) {
+            self.vue.images = data.images;
+            self.vue.has_more = data.has_more;
+            self.vue.logged_in = data.logged_in;
+            enumerate(self.vue.images);
+        });
+        self.vue.self_page = true;
+    };
+
+    self.get_more = function () {
+        var num_memos = self.vue.memos.length;
+        $.getJSON(get_images_url(num_memos, num_memos + 10), function (data) {
+            self.vue.has_more = data.has_more;
+            self.extend(self.vue.images, data.images);
+            enumerate(self.vue.images);
+        });
+    };
+
+    function get_user_images_url(user_email, start_idx, end_idx) {
+        var pp = {
+            user_email: user_email,
+            start_idx: start_idx,
+            end_idx: end_idx
+        };
+        return user_images_url + "?" + $.param(pp);
+    }
+
+    self.get_user_images = function (user_email) {
+        $.getJSON(get_user_images_url(user_email,0, 20), function (data) {
+            self.vue.images = data.images;
+            self.vue.has_more = data.has_more;
+            self.vue.logged_in = data.logged_in;
+            enumerate(self.vue.images);
+        });
+        self.vue.self_page = false;
+    };
+
+    self.get_users = function () {
+        $.getJSON(users_url, function (data) {
+            self.vue.users = data.users;
+            enumerate(self.vue.users);
+        });
+    };
+
+    self.open_uploader = function () {
+        $("div#uploader_div").show();
+        self.vue.is_uploading = true;
+    };
+
+    self.close_uploader = function () {
+        $("div#uploader_div").hide();
+        self.vue.is_uploading = false;
+        $("input#file_input").val(""); // This clears the file choice once uploaded.
+
+    };
+
+    self.upload_file = function (event) {
+        // Reads the file.
+        var input = event.target;
+        var file = input.files[0];
+        if (file) {
+            // First, gets an upload URL.
+            console.log("Trying to get the upload url");
+            $.getJSON('https://upload-dot-luca-teaching.appspot.com/start/uploader/get_upload_url',
+                function (data) {
+                    // We now have upload (and download) URLs.
+                    var put_url = data['signed_url'];
+                    var get_url = data['access_url'];
+                    console.log("Received upload url: " + put_url);
+                    // Uploads the file, using the low-level interface.
+                    var req = new XMLHttpRequest();
+                    req.addEventListener("load", self.upload_complete(get_url));
+                    // TODO: if you like, add a listener for "error" to detect failure.
+                    req.open("PUT", put_url, true);
+                    req.send(file);
+                });
+        }
+    };
+
+    self.upload_complete = function(get_url) {
+        // Hides the uploader div.
+        self.close_uploader();
+        console.log('The file was uploaded; it is now available at ' + get_url);
+        $.post(add_image_url,
+            {
+                image_url: get_url
+            },
+            function (data) {
+                setTimeout(function() {
+                    self.vue.images.unshift(data.image);
+                    enumerate(self.vue.images);
+                }, 1000);
+            });
+    };
+
+
     self.vue = new Vue({
         el: "#vue-div",
         delimiters: ['${', '}'],
         unsafeDelimiters: ['!{', '}'],
         data: {
+            images: [],
+            users: [],
             is_uploading: false,
-            self_page: true
+            self_page: true // Leave it to true, so initially you are looking at your own images.
         },
         methods: {
+            get_images: self.get_images,
+            get_more: self.get_more,
+            get_users: self.get_users,
+            get_user_images: self.get_user_images,
             open_uploader: self.open_uploader,
             close_uploader: self.close_uploader,
-            upload_file: self.upload_file
+            upload_file: self.upload_file,
         }
 
     });
-    $("#vue-div").show()
 
+    self.get_images();
+    self.get_users();
+    $("#vue-div").show();
     return self;
 };
 
